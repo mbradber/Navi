7715660d5569ca062180bd679c40ee99640ada39
Matthew Bradberry <mcbradberry@gmail.com>
2018-12-31 15:45:17 -0800

    init

---
+++ Navi/main.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <GL/gl3w.h>
+#include <GLFW/glfw3.h>
+
+int main()
+{
+	if (!glfwInit())
+	{
+		fprintf(stderr, "ERROR: could not start GLFW3\n");
+		return 1;
+	}
+
+	GLFWwindow *window = glfwCreateWindow(640, 480, "Navi", NULL, NULL);
+	if (!window)
+	{
+		fprintf(stderr, "ERROR: could not open window with GLFW3\n");
+		glfwTerminate();
+		return 1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	gl3wInit();
+
+	// get version info
+	const GLubyte *renderer = glGetString(GL_RENDERER);
+	const GLubyte *version = glGetString(GL_VERSION);
+	printf("Renderer: %s\n", renderer);
+	printf("OpenGL version supported %s\n", version);
+
+	glEnable(GL_DEPTH_TEST); // enable depth testing
+	glDepthFunc(GL_LESS); // depth-testing interprets a smaller value as "closer"
+
+	// define static triangle
+	GLfloat points[] = {
+		0.0f, 0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		-0.5f, -0.5f, 0.0f
+	};
+
+	GLuint vbo = 0;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
+
+	GLuint vao = 0;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+	const char* vertex_shader = 
+		"#version 410\n"
+		"in vec3 vp;"
+		"void main () {"
+		"	gl_Position = vec4 (vp, 1.0);" 
+		"}";
+
+	const char* fragment_shader =
+		"#version 410\n"
+		"out vec4 frag_colour;"
+		"void main () {"
+		"	frag_colour = vec4 (0.5, 0.0, 0.5, 1.0);"
+		"}";
+
+	// compile shaders and link into a program
+	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vs, 1, &vertex_shader, NULL);
+	glCompileShader(vs);
+
+	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fs, 1, &fragment_shader, NULL);
+	glCompileShader(fs);
+
+	GLuint shader_program = glCreateProgram();
+	glAttachShader(shader_program, fs);
+	glAttachShader(shader_program, vs);
+	glLinkProgram(shader_program);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glUseProgram(shader_program);
+		glBindVertexArray(vao);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glfwPollEvents();
+
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
+	return 0;
+}
